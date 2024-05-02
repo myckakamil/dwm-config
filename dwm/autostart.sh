a657fab807e2f1d9e246f9389e00ad89eb13c91a
Mordimmer <kamil.mycka@protonmail.com>
2024-05-02 23:34:39 +0200

    Cleaner autostart

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
 slstatus &
+picom &
+nm-applet &
+pasystray &
+blueman-applet &
+flameshot & 
+lxpolkit &
+feh --bg-fill --randomize /home/kamil/.config/wallpapers &
+nextcloud &
+
+#Touchpad xinput settings
+#Tap to click
+xinput set-prop 13 'libinput Tapping Enabled' 1
+
+#Natural scrolling
+xinput set-prop 13 'libinput Natural Scrolling Enabled' 1
