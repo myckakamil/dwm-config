07524c952a24db2fe3f9696fc685d562b0d295c6
Mordimmer <kamil.mycka@protonmail.com>
2024-05-03 21:39:04 +0200

    Code refactors + changed default layout to tilewide

--- dwm/config.h
+++ dwm/config.h
@@ -12,7 +12,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+//static const char dmenufont[]       = "monospace:size=10";
 
 static const char col_gray1[]       = "#2E3440";
 static const char col_gray2[]       = "#3B4252";
@@ -39,17 +39,17 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const float mfact     	= 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     	= 1;    /* number of clients in master area */
+static const int resizehints 	= 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[][]=",    tilewide },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "[][]=",    tilewide },
+	{ "[]=",      tile },    /* first entry is default */
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
 };
@@ -80,50 +80,50 @@ static const char *next[]       = { "/usr/bin/playerctl", "next", "toggle", NULL
 static const char *previous[]   = { "/usr/bin/playerctl", "previous", "toggle", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      spawn,          SHCMD ("firefox")},
-	{ 0,                            0xff61,    spawn,          SHCMD ("flameshot gui")},
-        { MODKEY,                       XK_e,      spawn,          SHCMD ("thunar")},
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
- 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
- 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[5]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ 0,                       0x1008ff11, spawn, {.v = downvol } },
-        { 0,                       0x1008ff12, spawn, {.v = mutevol } },
-        { 0,                       0x1008ff13, spawn, {.v = upvol   } },
-        { 0,                       0x1008ff14, spawn, {.v = play   } },
-        { 0,                       0x1008ff17, spawn, {.v = next   } },
-        { 0,                       0x1008ff16, spawn, {.v = previous   } },
+	/* modifier                     key        	function        argument */
+	{ MODKEY,                       XK_r,      	spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, 	spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      	spawn,          SHCMD ("firefox")},
+	{ 0,                            0xff61,    	spawn,          SHCMD ("flameshot gui")},
+    { MODKEY,                       XK_e,      	spawn,          SHCMD ("thunar")},
+	{ MODKEY,                       XK_j,      	focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      	focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      	incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      	incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      	setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      	setmfact,       {.f = +0.05} },
+ 	{ MODKEY|ShiftMask,             XK_j,      	movestack,      {.i = +1 } },
+ 	{ MODKEY|ShiftMask,             XK_k,      	movestack,      {.i = -1 } },
+	{ MODKEY,                       XK_Return, 	zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    	view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      	killclient,     {0} },
+	{ MODKEY,                       XK_t,      	setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      	setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      	setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_t,      	setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_y,      	setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_u,      	setlayout,      {.v = &layouts[5]} },
+	{ MODKEY,                       XK_space,  	setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  	togglefloating, {0} },
+	{ MODKEY,                       XK_0,      	view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      	tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  	focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 	focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  	tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 	tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_q, 		quit,           {0} },
+	{ 0,                     		0x1008ff11, spawn, 			{.v = downvol } },
+	{ 0,                       		0x1008ff12, spawn, 			{.v = mutevol } },
+	{ 0,                       		0x1008ff13, spawn, 			{.v = upvol   } },
+	{ 0,                       		0x1008ff14, spawn, 			{.v = play   } },
+	{ 0,                       		0x1008ff17, spawn, 			{.v = next   } },
+	{ 0,                       		0x1008ff16, spawn, 			{.v = previous   } },
+	TAGKEYS(                        XK_1,                      	0)
+	TAGKEYS(                        XK_2,                      	1)
+	TAGKEYS(                        XK_3,                      	2)
+	TAGKEYS(                        XK_4,                      	3)
+	TAGKEYS(                        XK_5,                      	4)
+	TAGKEYS(                        XK_6,                      	5)
 };
 
 /* button definitions */
@@ -142,4 +142,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
